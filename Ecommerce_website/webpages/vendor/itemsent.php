c9ca0b9151d27f72f2cdff3e82d80a3b8b83e43b
melisks <meliodassks2001@gmail.com>
2023-06-24 21:47:51 +0530

    just checking

--- Ecommerce_website/webpages/vendor/itemsent.php
+++ Ecommerce_website/webpages/vendor/itemsent.php
@@ -4,8 +4,10 @@ if ($_SESSION['login_state'] == true && $_SESSION['utype'] == "vendor") {
     $pid = $_GET['pid'];
     $uid = $_GET['uid'];
     include "../includes/sqlcon.php";
-    $status = "DELETE FROM order_info WHERE pid=$pid AND uid=$uid";
+    $status = "INSERT INTO order_received(complete) VALUE('yes')";
     $result = mysqli_query($con, $status);
+    $status = "DELETE FROM order_info WHERE pid=$pid AND uid=$uid";
+    $result1 = mysqli_query($con, $status1);
     if ($result) {
         header("location:ordermanage.php?cinfo=no");
     } else {
